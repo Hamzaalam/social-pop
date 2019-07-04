0deaacf9fa09b91ca98b7e2309c49ce26be5c325
Hamza Alam <getright_02@live.com>
2019-07-04 18:27:40 +0500

    login and signup page ready

--- client-app/src/app/app-routing.module.ts
+++ client-app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './home/sign-up/sign-up.component';
+import { LoginComponent } from './home/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'',redirectTo:'/home/login',pathMatch:'full'},
+    {path:'home',component:HomeComponent,
+     children:[
+        {path:'sign-up', component:SignUpComponent},
+        {path:'login', component:LoginComponent}
+     ] 
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import { SignUpComponent } from './home/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,

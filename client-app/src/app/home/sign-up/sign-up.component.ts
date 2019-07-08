40dba7e85fcf9714e41199a9173b4def6be4e8fc
Hamza Alam <getright_02@live.com>
2019-07-08 17:41:43 +0500

    validation of sign up completed

--- client-app/src/app/home/sign-up/sign-up.component.ts
+++ client-app/src/app/home/sign-up/sign-up.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
+  providers:[UserService]
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+  console.log(this.userService.selectedUser.name)
+  console.log(this.userService.selectedUser.phone)
+  console.log(this.userService.selectedUser.email)
+  console.log(this.userService.selectedUser.password)
   }
 
 }
